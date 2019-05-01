af5a85daf755d8a70a1ab7409072cb2b0b56c9db
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2019-04-30 22:57:35 -0500

    Interfaz de colección y el uso de métodos comunes

---
+++ Collections/src/main/java/com/CollectionConcepts.java
@@ -0,0 +1,60 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionConcepts {
+	public static void main(String[] args) {
+		Product door = new Product("Wooden Door", 35);
+		Product floorPanel = new Product("Floor Panel", 25);
+		Product window = new Product("Glass Window", 10);
+
+		Collection<Product> products = new ArrayList<>();
+		products.add(door);
+		products.add(floorPanel);
+		products.add(window);
+
+		// System.out.println(products);
+
+
+		// Hay cosas que se pueden hacer con un iterador y no con un foreach
+
+		// ConcurrentModificationException -> ocurre porque se está iterando sobre nuestra colección y re
+		// modificandolo al mismo tiempo.
+		// for (Product product : products) {
+		// 	 if (product.getWeight() > 20) {
+		// 		 System.out.println(product);
+		// 	 } else {
+		// 	 	 products.remove(product);
+		//		 products.add(window);
+		//		 products.clear();
+		// 	 }
+		// }
+
+
+		// Iterator
+		final Iterator<Product> productIterator = products.iterator();
+		while (productIterator.hasNext()) {
+			Product product = productIterator.next();
+			if (product.getWeight() > 20) {
+				System.out.println(product);
+			} else {
+				productIterator.remove();
+			}
+		}
+
+		System.out.println(products.size());
+		System.out.println(products.isEmpty());
+		System.out.println(products.contains(window));
+		System.out.println(products.contains(door));
+
+		Collection<Product> otherProducts = new ArrayList<>();
+		otherProducts.add(window);
+		otherProducts.add(door);
+
+		products.removeAll(otherProducts);
+
+		System.out.println(products);
+	}
+}
